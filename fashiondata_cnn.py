ffa44b75760dd21b386b6ef02786c74198c2eb89
Muhammet ÇAKICI <muh.cakici@gmail.com>
2022-06-15 22:34:51 +0300

    Add files via upload

---
+++ fashiondata_cnn.py
@@ -0,0 +1,55 @@
+import keras
+from keras.datasets import fashion_mnist 
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+# from keras import layers  
+from keras.models import Sequential
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+(train_X,train_Y), (test_X,test_Y) = fashion_mnist.load_data()
+
+train_X = train_X.reshape(-1, 28,28, 1)
+test_X = test_X.reshape(-1, 28,28, 1)
+
+# normalize eder (0-1 aralığına değerleri çeker)
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255
+test_X = test_X / 255
+
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+
+model = Sequential()
+model.add(Conv2D(32, (3,3), input_shape=(28, 28, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, 
+              optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.fit(train_X, train_Y_one_hot, batch_size=32, epochs=3)
+
+test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
+print('Test loss', test_loss)
+print('Test accuracy', test_acc)
+
+# predictions = model.predict(test_X)
+# print(np.argmax(np.round(predictions[0])))
+
+# plt.imshow(test_X[0].reshape(28, 28), cmap = plt.cm.binary)
+# plt.show()
+
+# import cv2
+
+# aa=train_X[1,:, :]
+# aa1=cv2.resize(aa,(128,128))
+# cv2.imshow("Image",aa1)
